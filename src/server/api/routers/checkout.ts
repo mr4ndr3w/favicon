8da3b52b44c75279e76b18902962300b28c388b5
mr4ndr3w <shakhmut.andrew@gmail.com>
2023-11-02 21:20:59 +0100

    initial commit

--- src/server/api/routers/checkout.ts
+++ src/server/api/routers/checkout.ts
@@ -16,7 +16,7 @@ export const checkoutRouter = createTRPCRouter({
       },
       line_items: [{ price: env.PRICE_ID, quantity: 1 }],
       mode: "payment",
-      success_url: `${env.HOST_NAME}/success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${env.HOST_NAME}/`,
       cancel_url: `${env.HOST_NAME}/`,
     });
   }),
