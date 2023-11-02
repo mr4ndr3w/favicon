1f2d101ae2e9597e56410058b868e23da612963e
mr4ndr3w <shakhmut.andrew@gmail.com>
2023-11-02 21:51:18 +0100

    initial commit

--- src/server/api/routers/checkout.ts
+++ src/server/api/routers/checkout.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 import { env } from "~/env.mjs";
 
 const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2023-10-16"
+  apiVersion: "2022-11-15"
 });
 
 export const checkoutRouter = createTRPCRouter({
