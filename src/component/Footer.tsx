64e9c4fb9511cfc2b8c57b70c16b5caa413caf9c
mr4ndr3w <shakhmut.andrew@gmail.com>
2023-11-12 19:45:01 +0100

    initial commit

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="dark:bg-gray-900">
       <div className="container mx-auto grid h-24 grid-cols-3 items-center bg-gray-900 text-center">
-        <PrimaryLink href="/">IconGenerator.com</PrimaryLink>
+        <PrimaryLink href="/">Favicon.com</PrimaryLink>
         <PrimaryLink href="/privacy-policy">Privacy Policy</PrimaryLink>
         <PrimaryLink href="/terms-of-service">Terms of Service</PrimaryLink>
       </div>
